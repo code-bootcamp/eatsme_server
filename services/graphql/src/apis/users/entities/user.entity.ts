5bb930d2a08c799aed9b623983bdc4d7d9af1379
onionsss11 <black1594@naver.com>
2023-03-16 19:51:10 +0900

    feat: 전체 entity 초기설정

---
+++ services/graphql/src/apis/users/entities/user.entity.ts
@@ -0,0 +1,41 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Alarm } from 'src/apis/alarm/entities/alarm.entity';
+import { Board } from 'src/apis/boards/entities/board.entity';
+import { Reservation } from 'src/apis/reservations/entities/reservation.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => String)
+  id: string;
+
+  @Column({ type: 'varchar', length: 36 })
+  @Field(() => String)
+  email: string;
+
+  @Column({ type: 'varchar', length: 20 })
+  // @Field(() => String)
+  password: string;
+
+  @Column({ type: 'varchar', length: 20 })
+  @Field(() => String)
+  nickname: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  @Field(() => String)
+  userImg: string;
+
+  @OneToMany(() => Reservation, (reservation) => reservation.users)
+  @Field(() => [Reservation])
+  reservations: Reservation[];
+
+  @OneToMany(() => Board, (board) => board.users)
+  @Field(() => [Board])
+  boards: Board[];
+
+  @OneToMany(() => Alarm, (alarm) => alarm.users)
+  @Field(() => [Alarm])
+  alarms: Alarm[];
+}
