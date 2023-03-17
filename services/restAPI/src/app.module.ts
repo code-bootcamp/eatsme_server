3de95341dc93d9f1c7354f8d0babdcc73281a7f3
HUMBLE25 <wlsrua1007@gmail.com>
2023-03-17 10:38:43 +0900

    feat: 식당정보api개발 기초설정

--- services/restAPI/src/app.module.ts
+++ services/restAPI/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RestaurantModule } from './apis/restaurant/restaurant.module';
 
 @Module({
   imports: [
+    RestaurantModule,
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MY_MONGODB), //
   ],
