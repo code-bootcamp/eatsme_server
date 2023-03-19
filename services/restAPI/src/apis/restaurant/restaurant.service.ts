69608003bfe82ee25284ee8ae3dc93eead5d494c
HUMBLE25 <wlsrua1007@gmail.com>
2023-03-19 17:17:48 +0900

    feat:식당 정보 저장 api중 식당정보 가져오는 로직 완성

--- services/restAPI/src/apis/restaurant/restaurant.service.ts
+++ services/restAPI/src/apis/restaurant/restaurant.service.ts
@@ -12,19 +12,16 @@ export class RestaurantService {
     section,
   }: IRestaurantServicePostRestaurant): Promise<void> {
     const apiKey = process.env.GOOGLE_MAP_API_KEY;
-    const type = 'restaurant';
     const config = {
       method: 'get',
-      url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${section}&type=${type}&key=${apiKey}&language=ko&opennow&fields=current_opening_hours`,
+      url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${section}&type=restaurant&key=${apiKey}&language=ko&opennow&fields=current_opening_hours`,
     };
     const result = await axios(config);
-    const nextPageToken = result.data.next_page_token;
-    const arr = result.data.results;
-    //DB에 반복저으로 저장한다.
-    await this.saveRepeat(arr);
-
+    //DB에 반복적으로 저장한다.
+    const restaurantsInfos = result.data.results;
+    await this.saveRepeat(restaurantsInfos);
     //다음페이지의 정보를 저장한다.
-    console.log(section);
+    const nextPageToken = result.data.next_page_token;
     this.saveNextPage({ nextPageToken, section });
   }
 
@@ -64,20 +61,19 @@ export class RestaurantService {
     return { formatted_phone_number, opening_hours };
   }
 
-  saveNextPage(nextPageToken, section: IRestaurantServiceSaveNextPage) {
+  saveNextPage({ nextPageToken, section }: IRestaurantServiceSaveNextPage) {
     const apiKey = process.env.GOOGLE_MAP_API_KEY;
-    const type = 'restaurant';
     const getNextRestaurant = async ({ nextPageToken }) => {
       if (nextPageToken) {
         const nextConfig = {
           method: 'get',
-          url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${section}&type=${type}&key=${apiKey}&language=ko&pagetoken=${nextPageToken}&opennow&fields=current_opening_hours`,
+          url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${section}&type=restaurant&key=${apiKey}&language=ko&pagetoken=${nextPageToken}&opennow&fields=current_opening_hours`,
         };
         //2초정도의 지연시간이 없으면 같은 정보를 받아오므로 setTimeout으로 지연시켜주었다.
         setTimeout(async () => {
           const result = await axios(nextConfig);
-          const arr = result.data.results;
-          await this.saveRepeat(arr);
+          const restaurantsInfos = result.data.results;
+          await this.saveRepeat(restaurantsInfos);
           const nextPageToken = result.data.next_page_token;
           if (nextPageToken) {
             return getNextRestaurant({ nextPageToken });
