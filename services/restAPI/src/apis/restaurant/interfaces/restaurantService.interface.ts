3c5b2322f0c53247d9f74d7e236df66b780ee115
HUMBLE25 <wlsrua1007@gmail.com>
2023-03-19 17:05:00 +0900

    feat:식당 정보 가져오는 로직 리팩토링

--- services/restAPI/src/apis/restaurant/interfaces/restaurantService.interface.ts
+++ services/restAPI/src/apis/restaurant/interfaces/restaurantService.interface.ts
@@ -1,3 +1,11 @@
 export interface IRestaurantServicePostRestaurant {
   section: string;
 }
+export interface IRestaurantServiceGetDetails {
+  place_id: string;
+}
+
+export interface IRestaurantServiceSaveNextPage {
+  nextPageToken: string;
+  section: string;
+}
--- services/restAPI/src/apis/restaurant/restaurant.service.ts
+++ services/restAPI/src/apis/restaurant/restaurant.service.ts
@@ -1,6 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import axios from 'axios';
-import { IRestaurantServicePostRestaurant } from './interfaces/restaurantService.interface';
+import {
+  IRestaurantServiceGetDetails,
+  IRestaurantServicePostRestaurant,
+  IRestaurantServiceSaveNextPage,
+} from './interfaces/restaurantService.interface';
 
 @Injectable()
 export class RestaurantService {
@@ -8,9 +12,7 @@ export class RestaurantService {
     section,
   }: IRestaurantServicePostRestaurant): Promise<void> {
     const apiKey = process.env.GOOGLE_MAP_API_KEY;
-
     const type = 'restaurant';
-
     const config = {
       method: 'get',
       url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${section}&type=${type}&key=${apiKey}&language=ko&opennow&fields=current_opening_hours`,
@@ -18,6 +20,16 @@ export class RestaurantService {
     const result = await axios(config);
     const nextPageToken = result.data.next_page_token;
     const arr = result.data.results;
+    //DB에 반복저으로 저장한다.
+    await this.saveRepeat(arr);
+
+    //다음페이지의 정보를 저장한다.
+    console.log(section);
+    this.saveNextPage({ nextPageToken, section });
+  }
+
+  saveRepeat(arr) {
+    let num = 0;
     arr.forEach(async (el) => {
       const {
         formatted_address,
@@ -27,22 +39,52 @@ export class RestaurantService {
         rating,
         user_ratings_total,
       } = el;
-      const placeConfig = {
-        method: 'get',
-        url: `https://maps.googleapis.com/maps/api/place/details/json?&key=${apiKey}&language=ko&place_id=${place_id}&fields=formatted_phone_number,opening_hours`,
-      };
-      const result2 = await axios(placeConfig);
-      const { formatted_phone_number, opening_hours } = result2.data.result;
+      const details = await this.getDetails(place_id);
+      num++;
       console.log(
-        formatted_phone_number,
-        opening_hours,
-        formatted_address,
-        geometry,
-        place_id,
+        'num:',
+        num,
         name,
+        formatted_address,
+        geometry.location,
         rating,
         user_ratings_total,
+        details,
       );
     });
+  }
+  async getDetails(place_id: IRestaurantServiceGetDetails): Promise<object> {
+    const apiKey = process.env.GOOGLE_MAP_API_KEY;
+    const placeConfig = {
+      method: 'get',
+      url: `https://maps.googleapis.com/maps/api/place/details/json?&key=${apiKey}&language=ko&place_id=${place_id}&fields=formatted_phone_number,opening_hours`,
+    };
+    const result = await axios(placeConfig);
+    const { formatted_phone_number, opening_hours } = result.data.result;
+    return { formatted_phone_number, opening_hours };
+  }
+
+  saveNextPage(nextPageToken, section: IRestaurantServiceSaveNextPage) {
+    const apiKey = process.env.GOOGLE_MAP_API_KEY;
+    const type = 'restaurant';
+    const getNextRestaurant = async ({ nextPageToken }) => {
+      if (nextPageToken) {
+        const nextConfig = {
+          method: 'get',
+          url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${section}&type=${type}&key=${apiKey}&language=ko&pagetoken=${nextPageToken}&opennow&fields=current_opening_hours`,
+        };
+        //2초정도의 지연시간이 없으면 같은 정보를 받아오므로 setTimeout으로 지연시켜주었다.
+        setTimeout(async () => {
+          const result = await axios(nextConfig);
+          const arr = result.data.results;
+          await this.saveRepeat(arr);
+          const nextPageToken = result.data.next_page_token;
+          if (nextPageToken) {
+            return getNextRestaurant({ nextPageToken });
+          }
+        }, 2000);
+      }
+    };
+    getNextRestaurant({ nextPageToken });
   }
 }
