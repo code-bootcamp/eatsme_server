eaf329b5d99e069de16a50a8f279b6f0e2474303
HUMBLE25 <wlsrua1007@gmail.com>
2023-03-23 14:24:27 +0900

    feat:restaurantService의 getRestaurants,deleteCollection,deleteSection 테스트코드 작성

--- services/restAPI/src/apis/restaurant/_test_/restaurant.service.spec.ts
+++ services/restAPI/src/apis/restaurant/_test_/restaurant.service.spec.ts
@@ -3,7 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { Model } from 'mongoose';
 import { IRestaurantServiceGetDetailsReturn } from '../interfaces/restaurantService.interface';
 import { RestaurantService } from '../restaurant.service';
-import { RestaurantDocument } from '../schemas/restaurant.schemas';
+import { Restaurant, RestaurantDocument } from '../schemas/restaurant.schemas';
 import { MockRestaurantDb } from './restaurant.mockDB';
 
 describe('RestaurantService', () => {
@@ -71,9 +71,9 @@ describe('RestaurantService', () => {
 
   describe('saveNextPage', () => {
     it('다음 페이지 토큰과 행정구역 정보를 받아 다음페이지의 식당 정보를 저장한다. 이때 반환값은 없다.', async () => {
-      const nextPageToken = '다음페이지의 정보를 요청할 수 있는 토큰';
+      const nextPageToken =
+        'ads678c8d7hd8h1fq#eqwf <=다음페이지의 식당정보를 요청할 수 있는 토큰';
       const section = '행정구역';
-
       jest
         .spyOn(restaurantService, 'saveNextPage')
         .mockImplementation(
@@ -84,6 +84,69 @@ describe('RestaurantService', () => {
       expect(
         await restaurantService.saveNextPage({ nextPageToken, section }),
       ).toBe(undefined);
+    });
+  });
+
+  describe('getRestaurants', () => {
+    it('등록된 행정구역을 입력할 경우 해당 행정구역의 식당 정보를 반환한다.', async () => {
+      const mockRestaurantDb = new MockRestaurantDb();
+      const body = '서울시 종로구';
+      jest
+        .spyOn(restaurantService, 'getRestaurants')
+        .mockImplementation(async ({ body }): Promise<Restaurant[]> => {
+          return mockRestaurantDb.restaurantInfo;
+        });
+      expect(await restaurantService.getRestaurants({ body })).toBe(
+        mockRestaurantDb.restaurantInfo,
+      );
+    });
+    // it('등록되지 않은 행정구역을 요청한경우');
+  });
+
+  describe('deleteCollection', () => {
+    it('발급받은 ID로 수정을 요청한 경우 삭제후 확인 메세지를 반환한다.', async () => {
+      const result = '정상적으로 지워졌습니다.';
+      const body = '64194481b8388f33864812b7';
+      jest
+        .spyOn(restaurantService, 'deleteCollection')
+        .mockImplementation(async ({ body }) => {
+          return result;
+        });
+      expect(await restaurantService.deleteCollection({ body })).toBe(result);
+    });
+    it('이미 지워진 ID로 수정을 요청한 경우 삭제후 확인 메세지를 반환한다.', async () => {
+      const result = '이미 지워진 collection입니다.';
+      const body = '64194481b8388f33864812bc';
+      jest
+        .spyOn(restaurantService, 'deleteCollection')
+        .mockImplementation(async ({ body }) => {
+          return result;
+        });
+      expect(await restaurantService.deleteCollection({ body })).toBe(result);
+    });
+  });
+
+  describe('deleteSection', () => {
+    it('존재하는 행정구역으로 요청한 경우 정상적으로 삭제한 경우 확인 메세지를 반환한다.', async () => {
+      const body = '서울시 강남구';
+      const result = '14개의 식당 정보를 정상적으로 지웠습니다.';
+      jest
+        .spyOn(restaurantService, 'deleteSection')
+        .mockImplementation(async ({ body }) => {
+          return result;
+        });
+      expect(await restaurantService.deleteSection({ body })).toBe(result);
+    });
+
+    it('이미 지워진 행정구역으로 요청한 경우 확인 메세지를 반환한다. ', async () => {
+      const body = '서울시 중구';
+      const result = '이미 지워진 행정구역입니다.';
+      jest
+        .spyOn(restaurantService, 'deleteSection')
+        .mockImplementation(async ({ body }) => {
+          return result;
+        });
+      expect(await restaurantService.deleteSection({ body })).toBe(result);
     });
   });
 });
