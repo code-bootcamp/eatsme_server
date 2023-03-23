89005f57e8552b585ed9d103183529ebbff2a9f3
HUMBLE25 <wlsrua1007@gmail.com>
2023-03-23 18:04:06 +0900

    feat: Restaurantcontorller.deleteRestaurant의 오류 테스트 코드 작성성

--- services/restAPI/src/apis/restaurant/_test_/restaurant.controller.spec.ts
+++ services/restAPI/src/apis/restaurant/_test_/restaurant.controller.spec.ts
@@ -1,4 +1,4 @@
-import { ForbiddenException, HttpException } from '@nestjs/common';
+import { ForbiddenException, HttpException, HttpStatus } from '@nestjs/common';
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Model, MongooseError } from 'mongoose';
@@ -80,15 +80,27 @@ describe('RestaurantController', () => {
         });
       expect(await restaurantController.deleteRestaurant(body)).toBe(result);
     });
-    // it('잘못된 ID로 수정을 요청한 경우 삭제후 확인 메세지를 반환한다.', async () => {
-    //   const body =
-    //     '64194481b8388f33864812b잘못된경우 길거나 짧은 경우 없는 아이디 입력한 경우';
-    //   jest.spyOn(restaurantService, 'deleteCollection');
-
-    //   await expect(async () => {
-    //     await restaurantController.deleteRestaurant(body);
-    //   }).rejects.toThrowError(HttpException);
-    // });
+    it('잘못된 ID로 수정을 요청한 경우 삭제후 확인 메세지를 반환한다.', async () => {
+      const body =
+        '64194481b8388f33864812b잘못된경우 길거나 짧은 경우 없는 아이디 입력한 경우';
+      jest
+        .spyOn(restaurantService, 'deleteCollection')
+        .mockRejectedValue(
+          new HttpException(
+            '잘못된 ID 형식입니다. 발급 받은 ID를 입력해주세요',
+            HttpStatus.BAD_REQUEST,
+          ),
+        );
+      const result = async () => {
+        await restaurantService.deleteCollection({ body });
+      };
+      await expect(result).rejects.toThrowError(
+        new HttpException(
+          '잘못된 ID 형식입니다. 발급 받은 ID를 입력해주세요',
+          HttpStatus.BAD_REQUEST,
+        ),
+      );
+    });
   });
   //!------deleteRestaurants-----!//
   describe('deleteRestaurants', () => {
