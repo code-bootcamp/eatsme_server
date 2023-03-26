4a28a1046e7bd4d54ed4c2b60389d32c4a4df5d1
sounwoo <mna800@naver.com>
2023-03-26 13:50:06 +0900

    test : HTTPS 배포환경 strict-origin-when-cross-origin 추가3

--- services/graphql/src/main.ts
+++ services/graphql/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { HttpExceptionFilter } from 'src/commons/filter/http-exception.filter';
 import { AppModule } from './app.module';
-import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,7 +9,6 @@ async function bootstrap() {
     origin: process.env.ORIGIN,
     credentials: true,
   });
-  app.use(helmet.referrerPolicy({ policy: 'strict-origin-when-cross-origin' }));
   await app.listen(7000, () => {
     console.log('=================');
     console.log('🐶🐶🐶 graphql 백엔드 서버 오픈 🐶🐶🐶');
