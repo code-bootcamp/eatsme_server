2bc3512b26da43592253e76c60a5b3da7b023a68
sounwoo <mna800@naver.com>
2023-04-03 19:04:11 +0900

    refactor : reply.service.ts 코드 리팩토링

--- services/graphql/src/apis/replies/reply.service.ts
+++ services/graphql/src/apis/replies/reply.service.ts
@@ -38,11 +38,11 @@ export class ReplysService {
       where: {
         id: replyId,
       },
-      relations: ['comments.board.user'],
+      relations: ['comments.board.user', 'comments.user', 'user'],
     });
   }
 
-  async nullCheck({ reply }: IReplyServiceNullList): Promise<void> {
+  nullCheck({ reply }: IReplyServiceNullList): void {
     // 댓글을 입력했는지 확인
     if (!reply.trim()) {
       throw new UnprocessableEntityException('댓글을 입력해주세요');
@@ -55,14 +55,11 @@ export class ReplysService {
   async checkUser({ userId, replyId }: IReplyServiceCheckUser): Promise<void> {
     const user = await this.usersService.findOneByUser({ userId });
 
-    const replies = await this.replysRepository.findOne({
-      where: { id: replyId },
-      relations: ['user'],
-    });
+    const replies = await this.findOne({ replyId });
+
     if (!replies) {
       throw new NotFoundException('댓글정보가 일치하지않습니다');
-    }
-    if (user.id !== replies.user.id) {
+    } else if (user.id !== replies.user.id) {
       throw new NotFoundException('댓글 작성자가 아닙니다');
     }
   }
@@ -83,19 +80,14 @@ export class ReplysService {
     if (!comments) {
       throw new NotFoundException('현재 없는 댓글 입니다');
     }
-    await this.nullCheck({ reply });
+
+    this.nullCheck({ reply });
+
     const newComment = await this.replysRepository.save({
       ...createReplyInput,
       comments,
       user,
     });
-
-    const newAlarm = this.alarmsRepository.create({
-      users: comments.board.user,
-      replies: newComment,
-      alarmMessage: `${newComment.user.nickname}님이 대댓글을 작성했습니다`,
-    });
-    await this.alarmsRepository.save(newAlarm);
 
     const boardUser = comments.board.user;
     const commentUser = comments.user;
@@ -113,15 +105,15 @@ export class ReplysService {
       usersToSendAlarm = [boardUser];
     }
     // 두 명의 사용자에게 각각 알람을 보냅니다.
-    for (const user of usersToSendAlarm) {
-      const newAlarm = this.alarmsRepository.create({
-        users: user,
+    usersToSendAlarm.forEach(async (el) => {
+      await this.alarmsRepository.save({
+        users: el,
         replies: newComment,
         commentUserImg: newComment.user.userImg,
         alarmMessage: `${newComment.user.nickname}님이 대댓글을 작성했습니다`,
       });
-      await this.alarmsRepository.save(newAlarm);
-    }
+    });
+
     return newComment;
   }
 
@@ -131,37 +123,22 @@ export class ReplysService {
     updateReplyInput,
   }: IReplysServiceUpdate): Promise<Reply> {
     const { reply, replyId } = updateReplyInput;
-    const replies = await this.replysRepository.findOne({
-      where: { id: replyId },
-      relations: ['user', 'comments.user', 'comments.board.user'],
-    });
+    const replies = await this.findOne({ replyId });
+
     await this.checkUser({ userId, replyId });
-    await this.nullCheck({ reply });
-    const updateComment = await this.replysRepository.save({
+
+    this.nullCheck({ reply });
+
+    return this.replysRepository.save({
       ...replies,
       reply,
     });
-
-    const boardUser = replies.comments.board.user;
-    const commentUser = replies.comments.user;
-    const usersToSendAlarm = [commentUser, boardUser];
-
-    for (const user of usersToSendAlarm) {
-      // user상수를 만들어서 usersToSendAlarm배열 안에 있는 값에 각각요소를 user에 할당후 알람을 보냄
-      const updateAlarm = this.alarmsRepository.create({
-        users: replies.user,
-        replies: updateComment,
-        commentUserImg: updateComment.user.userImg,
-        alarmMessage: `${updateComment.user.nickname}님이 대댓글을 수정했습니다`,
-      });
-      await this.alarmsRepository.save(updateAlarm);
-    }
-    return updateComment;
   }
 
   async delete({ userId, replyId }: IReplysServiceDelete): Promise<string> {
     await this.checkUser({ replyId, userId });
     const reply = await this.replysRepository.delete(replyId);
+
     return reply.affected ? '데이터삭제' : '데이터없음';
   }
 }
